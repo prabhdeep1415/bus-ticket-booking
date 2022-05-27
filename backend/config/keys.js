4488fc4fba46b02a3ad05101d79a32473726e30d
Prabhdeep <uietcse1923prabhdeepkaur@kuk.ac.in>
2022-05-27 12:30:52 +0530

    initial commit

---
+++ backend/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // MongoURI: "mongodb+srv://AdityaNew:123@cluster0-rsoqu.mongodb.net/test?retryWrites=true&w=majority"
+    MongoURI: "mongodb+srv://akshaythappar:s4u0JGnzBQJuoMJM@cluster0.j02bh.mongodb.net/student-api?retryWrites=true&w=majority"
+}
